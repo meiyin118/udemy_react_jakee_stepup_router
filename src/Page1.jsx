69a11885b4db751c23e7fd467be6249931b68b7c
Harune Odagiri <harune.odagiri@nijibox.co.jp>
2023-01-03 12:02:49 +0000

    useLocation()のstateを使用して、画面遷移時に必要な情報を受け渡す

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 export const Page1 = () => {
+  // 配列をdetailAのページに画面遷移した際に情報を渡す
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="/page1/detail-a">詳細A</Link>
+      {/* <Link to="/page1/detail-a">詳細A</Link> */}
+      <Link to={{ pathname: "/page1/detail-a", state: arr }}>詳細A</Link>
       <br />
       <Link to="/page1/detail-b">詳細B</Link>
       <br />
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,9 @@
+// useLocationでstateを取得する
+import { useLocation } from "react-router-dom";
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  // 前ページから受け取ったstateを表示
+  console.log(state);
   return (
     <>
       <h1>Page1の詳細ページAです</h1>
