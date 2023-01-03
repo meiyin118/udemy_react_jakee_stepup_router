e5a781fd9a7d113fc1b0b76e1bac159aa0c2f287
Harune Odagiri <harune.odagiri@nijibox.co.jp>
2023-01-03 12:36:12 +0000

    useHistory()でjsで画面遷移をさせる

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 export const Page1 = () => {
   // 配列をdetailAのページに画面遷移した際に情報を渡す
   const arr = [...Array(100).keys()];
   console.log(arr);
+  const history = useHistory();
+  const onClickDetailA = () => {
+    // pushの引数にルートを指定してあげることで、JS側からリンクで遷移したような画面遷移をさせる
+    history.push("/page1/detail-a");
+  };
   return (
     <div>
       <h1>Page1</h1>
@@ -11,6 +16,7 @@ export const Page1 = () => {
       <br />
       <Link to="/page1/detail-b">詳細B</Link>
       <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
