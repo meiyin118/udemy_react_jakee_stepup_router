44fa42a10a67d8bd7a956fef1f3fcad7fecbc813
Harune Odagiri <harune.odagiri@nijibox.co.jp>
2023-01-03 12:44:20 +0000

    Linkのpathにアスタリスクを指定する事で、該当しないページは404ページに遷移させる

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
 import { Page1Routes } from "./Page1Routes";
 import { Page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -20,51 +21,56 @@ export const Router = () => {
       <Route
         path="/page1"
         render={({ match: { url } }) => (
-            // propsにルーティングに関わる情報が入ってくる
-            // この中の、match.urlを使えばわざわざpage1を下層ページ文書く必要がなくなる
-            // console.log(props)
-            //   console.log(url)
-            // 切り出す際に、何が違うか？を考える。（path/exact/レンダリングするコンポーネント）
-            //   <Switch>
-            //     <Route exact path={url}>
-            //       <Page1 />
-            //     </Route>
-            //     <Route path={`${url}/detail-a`}>
-            //       <Page1DetailA />
-            //     </Route>
-            //     <Route path={`${url}/detail-b`}>
-            //       <Page1DetailB />
-            //     </Route>
-            //   </Switch>
-            <Switch>
-                {Page1Routes.map((route) => (
-                    <Route
-                        key={route.path}
-                        exact={route.exact}
-                        path={`${url}${route.path}`}
-                    >
-                    {route.children}
-                    </Route>
-                ))}
-            </Switch>
+          // propsにルーティングに関わる情報が入ってくる
+          // この中の、match.urlを使えばわざわざpage1を下層ページ文書く必要がなくなる
+          // console.log(props)
+          //   console.log(url)
+          // 切り出す際に、何が違うか？を考える。（path/exact/レンダリングするコンポーネント）
+          //   <Switch>
+          //     <Route exact path={url}>
+          //       <Page1 />
+          //     </Route>
+          //     <Route path={`${url}/detail-a`}>
+          //       <Page1DetailA />
+          //     </Route>
+          //     <Route path={`${url}/detail-b`}>
+          //       <Page1DetailB />
+          //     </Route>
+          //   </Switch>
+          <Switch>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
         )}
       />
       <Route
         path="/page2"
-        render={({match:{ url }}) => (
-            <Switch>
-                {Page2Routes.map((route) => (                
-                    <Route 
-                        key={route.path}
-                        exact={route.exact}
-                        path={`${url}${route.path}`}
-                    >
-                        {route.children}
-                    </Route>
-                ))}
-            </Switch>
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
         )}
       />
+      <Route
+        path="*" // *は全て一致(＝指定したパス以外のもの)
+      >
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
